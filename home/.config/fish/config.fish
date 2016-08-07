18186fbfba03a6ca78beaa3714b135627f6f048a
SAWADA Tadashi <cesare@mayverse.jp>
2016-08-07 13:50:58 +0900

    Add config.fish

---
+++ home/.config/fish/config.fish
@@ -0,0 +1,13 @@
+set -U fish_user_paths ~/.rbenv/bin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin ~/bin
+
+alias be "bundle exec"
+
+alias rm "rm -i"
+alias mv "mv -i"
+alias cp "cp -i"
+
+rbenv init - | source
+
+function fish_user_key_bindings
+  bind \c] peco_select_ghq_repository
+end
