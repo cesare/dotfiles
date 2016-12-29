ac6c14273e6b3753e952f6fbd644896c49540cbb
SAWADA Tadashi <cesare@mayverse.jp>
2016-12-29 18:31:23 +0900

    Enable pyenv

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,5 +1,7 @@
 set -x GOPATH ~/src/golang
-set -U fish_user_paths {$GOPATH}/bin ~/.rbenv/bin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin ~/bin
+set -x PYENV_ROOT ~/.pyenv
+
+set -U fish_user_paths {$GOPATH}/bin ~/.rbenv/bin {$PYENV_ROOT}/bin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin ~/bin
 
 set -x EDITOR nvim
 set -x LESS "-i -M -R -S -W"
@@ -13,6 +15,10 @@ alias mv "mv -i"
 alias cp "cp -i"
 
 rbenv init - | source
+
+if test -d ~/.pyenv
+  pyenv init - | source
+end
 
 function fish_user_key_bindings
   bind \c] peco_select_ghq_repository
