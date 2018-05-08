2153ed06848227771a104e3388a4a82d6e248f88
SAWADA Tadashi <cesare@mayverse.jp>
2018-05-08 17:54:03 +0900

    Use fundle

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -5,8 +5,18 @@ set -x EDITOR nvim
 set -x LESS "-i -M -R -S -W"
 set -x XDG_CONFIG_HOME ~/.config
 set -x RSTUDIO_WHICH_R /usr/local/bin/R
+set -x FZF_DEFAULT_OPTS --reverse
 
 set -x PATH {$GOPATH}/bin ~/.rbenv/bin {$PYENV_ROOT}/bin ~/.cargo/bin ~/.local/bin /Library/TeX/texbin /usr/local/bin /usr/local/sbin /usr/bin /usr/sbin /bin /sbin ~/bin
+
+#
+# fundle
+#
+fundle plugin hagiyat/anyfff
+fundle init
+
+set -gx ANYFFF__FINDER_APP fzf
+
 
 function peco_select_project
   lpj --project-root={$HOME}/labs | fzf --reverse | read line
